4c431c72972234027d642d96efcdd5f240ef0a96
Carmelo Piccione <carmelo.piccione@struktured.io>
2018-03-11 15:32:29 -0400

    hmac test

--- test/auth.ml
+++ test/auth.ml
@@ -2,10 +2,6 @@ open Gemini
 open V1
 
 let api_secret = "1234abcd"
-let result =
-  "ewogICAgICJyZXF1ZXN0IjogIi92MS9vcmRlci9zdGF0dXMiLAogICAgICJub25jZSI6IDEyMzQ1NiwKCiAgICAgIm9yZGVyX2lkIjogMTg4MzQKfQo=-"
-
-let noonce () = Pipe.of_list [123456]
 
 let request_str =
 {|{
@@ -17,18 +13,23 @@ let request_str =
 |}
 
 let hex_encoding_test () =
-let noonce = noonce () in
-(*let request = Order.Status.{order_id=18834} in
-  let yojson = Order.Status.request_to_yojson request in
-  Request.make ~request:"/v1/order/status" ~noonce yojson >>= fun request -> *)
-  let payload_str = request_str
-(*    Request.to_yojson request |>
-    Yojson.Safe.to_string in *) in
-  let hex = Auth.of_payload payload_str in
+  let hex = Auth.of_payload request_str in
   printf "%s" (Auth.to_string hex);
   Deferred.unit
+
+
+let hmac384_encrypting_test () =
+  let hex = Auth.of_payload request_str in
+  let hmac = Auth.hmac_sha384 ~api_secret hex in
+  let str = Auth.to_string hmac in
+  printf "%s" str;
+  Deferred.unit
+
 
 let%expect_test "hex encoding" =
   hex_encoding_test () >>= fun () ->
   [%expect "ewogICAgICJyZXF1ZXN0IjogIi92MS9vcmRlci9zdGF0dXMiLAogICAgICJub25jZSI6IDEyMzQ1NiwKCiAgICAgIm9yZGVyX2lkIjogMTg4MzQKfQo=-"]
 
+let%expect_test "hmac384 encrypting" =
+  hmac384_encrypting_test () >>= fun () ->
+  [%expect "337cc8b4ea692cfe65b4a85fcc9f042b2e3f702ac956fd098d600ab15705775017beae402be773ceee10719ff70d710f"]
