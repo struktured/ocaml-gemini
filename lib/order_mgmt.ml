090735c13b58eb4369adf30cf9f0426167b4eb8b
Carmelo Piccione <carmelo.piccione@holo.host>
2020-11-30 01:34:01 -0500

    More order mgmt modules

--- lib/order_mgmt.ml
+++ lib/order_mgmt.ml
@@ -3,6 +3,8 @@ module Side = Market_data.Side
 
 module Change_event = Market_data.Change_event
 
+
+
 module Order = struct
   type t = { currency: Currency.t;
              side: Side.Bid_ask.t;
@@ -22,7 +24,7 @@ module By_side_price = Map.Make(Side_price)
 
 
 module Order_book = struct
-  type t = { book:Order.t By_side_price.t; currency: Currency.t }
+  type t = { book:Order.t By_side_price.t; currency: Currency.t } [@@deriving sexp, compare]
 
   let empty currency : t = { book=By_side_price.empty; currency}
 
@@ -80,4 +82,42 @@ module Order_book = struct
  (*    let request : Gemini.V1.Order.New.request = { !
     Gemini.V1.Order.New.post (module Cfg) *)
 
+  let replace (t:t) (order:Order.t) = 
+    {t with book = By_side_price.update t.book {side=order.side; price=order.price} ~f:(function | None | Some _ -> order)}
+
 end
+
+
+module Order_books = struct 
+  module Map = Map.Make(Currency)
+
+  type t = Order_book.t Map.t [@@deriving sexp, compare]
+
+  let empty = Map.empty
+
+  let set (t:t) (order:Order.t) = 
+    Map.update t order.currency ~f:
+      (fun order_book -> 
+        let order_book = Option.value order_book ~default:(Order_book.empty order.currency) in
+        Order_book.replace order_book order
+      )
+
+  let update (t:t) currency (change_event:Change_event.t) =
+    Map.update t currency ~f:
+      (fun order_book ->
+        let order_book = Option.value order_book ~default:(Order_book.empty currency) in
+        Order_book.update order_book change_event
+      )
+
+  let order_book (t:t) (currency:Currency.t) = Map.find t currency
+end
+
+module State = struct
+  type t = { private_book: Order_books.t ; public_book: Order_books.t } [@@deriving sexp, compare]
+
+
+  let empty = { private_book= Order_books.empty; 
+                public_book= Order_books.empty 
+              }
+end
+
